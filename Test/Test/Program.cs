3e29fda93afac3cb4167c7bee5fcb88cee6f02f4
Duc238 <92005265+Duc238@users.noreply.github.com>
2022-12-25 13:54:57 +0700

    Add files via upload

---
+++ Test/Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        public static void ChuanHoa(string s)
+        {
+            s = s.Trim();  // Xóa đầu cuối
+            Regex trimmer = new Regex(@"\s\s+"); // Xóa khoảng trắng thừa trong chuỗi
+            s = trimmer.Replace(s, " ");
+            Console.Write(s);
+        }
+        static void Main(string[] args)
+        {
+            ChuanHoa("\"                                           tran                                                                                                         ");
+            Console.ReadKey();
+        }
+    }
+}
